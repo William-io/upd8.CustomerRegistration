2e807fcd75348b80dd3be298cb4db56f233d4e3a
William-io <capuletos@live.com>
2022-10-27 14:02:23 -0300

    Atualizado

--- Source/upd8.CustomerRegistration.Domain/Entities/Customer.cs
+++ Source/upd8.CustomerRegistration.Domain/Entities/Customer.cs
@@ -1,9 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
 
 namespace upd8.CustomerRegistration.Domain.Entities
 {
     public class Customer
     {
+
         public void GenerateNewId()
         {
             Id = Guid.NewGuid();
@@ -11,12 +15,17 @@ namespace upd8.CustomerRegistration.Domain.Entities
 
         [Key]
         public Guid Id { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; } = null!;
+        [Required]
+        [MaxLength(11)]
         public string CPF { get; set; } = null!;
         public string BirthDate { get; set; } = null!;
         public string Sex { get; set; } = null!;
         public string Address { get; set; } = null!;
         public string State { get; set; } = null!;
         public string City { get; set; } = null!;
+
     }
 }
--- Source/upd8.CustomerRegistration.Infrastructure/Data/DataContext.cs
+++ Source/upd8.CustomerRegistration.Infrastructure/Data/DataContext.cs
@@ -1,12 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Flunt.Notifications;
+using Microsoft.EntityFrameworkCore;
 using upd8.CustomerRegistration.Domain.Entities;
 
 namespace upd8.CustomerRegistration.Infrastructure.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions options) : base(options)
+        public DataContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
         {
+           
         }
 
         public DbSet<Customer> Customers { get; set; } = null!;
