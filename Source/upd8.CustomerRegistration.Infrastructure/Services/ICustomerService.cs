e8301b44cc4ccc9d82726c21715f2b02a533334d
William-io <capuletos@live.com>
2022-10-27 13:41:00 -0300

    Finalizado

---
+++ Source/upd8.CustomerRegistration.Infrastructure/Services/ICustomerService.cs
@@ -0,0 +1,13 @@
+﻿using upd8.CustomerRegistration.Domain.Entities;
+
+namespace upd8.CustomerRegistration.Infrastructure.Services
+{
+    public interface ICustomerService
+    {
+        Task Add(Customer customer);
+        Task Update(Customer customer);
+        Task Delete(Customer customer);
+        Task<Customer> GetBy(Guid id);
+        Task<List<Customer>> GetAll();
+    }
+}
